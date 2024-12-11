e76337b38ddce9a0179b5c8672e20e22406aecf1
Mark-andrew7 <kamaumark19@gmail.com>
2024-12-11 07:30:51 +0000

    Correction of syntax errors in User and authRoutes

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
-const mongoose = require(mongoose);
-const bcrypt = require(bcrypt);
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
-const userSchema = new mongoose.schema({
+const userSchema = new mongoose.Schema({
     name: { type: String, required:true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
